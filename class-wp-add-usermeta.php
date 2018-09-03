c6de0550544aabd0fb86b45e5fa628aebbfd33de
january <january@january.local>
2018-09-04 00:07:19 +0900

    fix typo

--- class-wp-add-usermeta.php
+++ class-wp-add-usermeta.php
@@ -12,7 +12,7 @@ Text Domain:  wporg
 Domain Path:  /wp_add_usermeta
 */
 
-/*
+
 class WP_Add_UserMeta {
 	
 	function __construct() {
@@ -21,7 +21,7 @@ class WP_Add_UserMeta {
 	}
 
 	function activate() {
-		add_filter( 'user_contactmethods', array($this, 'modify_user_contact_methods'));
+		add_filter( 'user_contactmethods', array($this, 'add_usermeta_contacts'));
 	}
 
 	function deactivate() {
@@ -45,14 +45,3 @@ class WP_Add_UserMeta {
 }
 
 new WP_Add_UserMeta();
-*/
-
-
-	function add_usermeta_contacts($user_contact) {
-		$user_contact['qiita'] = __('Qiita URL');
-		$user_contact['github'] = __('GitHub URL');
-
-		return $user_contact;
-	}
-
-			add_filter( 'user_contactmethods', 'modify_user_contact_methods');
